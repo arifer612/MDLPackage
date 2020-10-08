f2dd387497e94c859a0a6fc596e539c23df9b8b4
arifer@linux <arifer1995@gmail.com>
2020-10-08 23:16:11 +0800

    Cleaned up code

--- mdl/main.py
+++ mdl/main.py
@@ -20,11 +20,10 @@ class User(object):
             self.search()
 
     def search(self, keyword=None, result=None, link=None):
-        if self.link:
-            self.link = self.link if not link else link
-        elif self.keyword:
+        self.link = self.link if not link else link
+        if not self.link and self.keyword:
             self.link = library.search(self.keyword if not keyword else keyword, self.result if not result else result)
-        elif not (keyword or link):
+        elif not keyword:
             print('Error: Provide either a search keyword or the link to the show')
         else:
             self.link = library.search(keyword, result)
@@ -92,7 +91,7 @@ class User(object):
 
 
 class Show(User):
-    def __init__(self, link=None, keyword=None, result=None):
+    def __init__(self, keyword=None, result=None, link=None):
         if not (keyword or link):
             sys.exit('Error: Provide either a search keyword or the link to the show')
-        super().__init__(link, keyword, result)
+        super().__init__(keyword=keyword, result=result, link=link)
